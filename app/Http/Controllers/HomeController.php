d0748b7ca678aab548008d19d27acd47d5be9292
Sherlockboy <kabilovhurshid5@gmail.com>
2020-12-06 13:46:09 +0500

    Home page finished

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        return view('public.home');
     }
 
     public function redirect()
